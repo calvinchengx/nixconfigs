ff1bdf5032e93ed77726c6ed560be68fb6b88f1e
Calvin Cheng <linchuan.cheng@gmail.com>
2015-06-03 15:56:46 +0000

    postgresql service

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,11 @@
     postgresql = {
         enable = true;
         package = pkgs.postgresql94;
-
+        authentication = pkgs.lib.mkOverride 10 ''
+            local all all              trust
+            host  all all 127.0.0.1/32 md5
+            host  all all ::1/128      md5
+        '';
     };
 
   };
